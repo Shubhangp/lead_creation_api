c6090a1cc0705d8ad587f68b820518bd98ac12f1
shubhangp <shubhanggpandey@gmail.com>
2025-03-21 19:59:18 +0530

    integrate fatakpay

--- controllers/leadController.js
+++ controllers/leadController.js
@@ -541,7 +541,7 @@ exports.createLead = async (req, res) => {
 
     // If source is not "FATAKPAY", send lead to external API
     const validPincodes = pinCodeData.map((row) => parseInt(row.Pincode, 10));
-    console.log(validPincodes);
+    // console.log(validPincodes);
     
     if (source !== 'FATAKPAY' && validPincodes.includes(parseInt(pincode))) {
       try {
