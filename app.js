398eaae4f490c1371126c247563de4967f0fecb9
Shubhangp <shubhanggpandey@gmail.com>
2025-12-26 19:35:49 +0530

    commit to apply previous commit

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 
 // Root route to respond with "Hello from Rate Cut"
 app.get('/', (req, res) => {
-    res.status(200).send('Hello from Rate Cut');
+    res.status(200).send('Hello from Rate Cut!');
 });
 
 app.use('/api/v1/leads', leadsRouter);
